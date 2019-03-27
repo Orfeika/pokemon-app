ecaf14379bb5d33a843fb0ba0c690dfdbe890fd3
Orfeika <tolstenkova.a.p@gmail.com>
2019-03-27 16:33:12 +0100

    changed styles for loading

--- src/components/common/Loading.js
+++ src/components/common/Loading.js
@@ -1,6 +1,15 @@
 import React from "react";
 import "./loading.css";
 
-const Loading = () => <div className="loader">Loading ... </div>;
+const Loading = () => {
+  return (
+    <div className="ball">
+      <div className="white-ball">
+        <div className="white-ball-small" />
+      </div>
+      <div class="top" />
+    </div>
+  );
+};
 
 export default Loading;
